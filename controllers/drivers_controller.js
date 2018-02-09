8e62ba0ecf7f089f20e074981e9cba156e93b0a0
Toucher-F <lianduanb@gmail.com>
2018-02-09 17:01:55 -0500

    add geometry find

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -5,6 +5,26 @@ module.exports = {
     res.send({ hi: "there" });
   },
 
+  index(req, res, next) {
+    // 'http://google.com?lng=80&lat=20'
+    const { lng, lat } = req.query;
+
+    Driver.find({
+      "geometry.coordinates": {
+        $nearSphere: {
+          $geometry: {
+            type: "Point",
+            coordinates: [parseFloat(lng), parseFloat(lat)]
+          },
+          $maxDistance: 200000
+        }
+      }
+    })
+
+      .then(drivers => res.send(drivers))
+      .catch(next);
+  },
+
   create(req, res, next) {
     const driverProps = req.body;
     Driver.create(driverProps)
